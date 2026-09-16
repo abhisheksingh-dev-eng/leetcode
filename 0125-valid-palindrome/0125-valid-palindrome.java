3e2e6a5579c8e2a91aae47bccb0552ad1122e7a8
abhisheksingh-dev-eng <abhisheks91525@gmail.com>
2026-09-16 10:36:16 +0530

    Time: 2 ms (99.3%), Space: 43.9 MB (95.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,29 +1,60 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+
+//         // Convert to lowercase
+//         s = s.toLowerCase();
+
+//         // Remove non-alphanumeric characters
+//         String clean = "";
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+
+//             if ((ch >= 'a' && ch <= 'z') || 
+//                 (ch >= '0' && ch <= '9')) {
+//                 clean = clean + ch;
+//             }
+//         }
+
+//         // Reverse the string
+//         String reverse = "";
+
+//         for (int i = clean.length() - 1; i >= 0; i--) {
+//             reverse = reverse + clean.charAt(i);
+//         }
+
+//         // Compare
+//         return clean.equals(reverse);
+//     }
+//
 class Solution {
     public boolean isPalindrome(String s) {
 
-        // Convert to lowercase
-        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
 
-        // Remove non-alphanumeric characters
-        String clean = "";
+        while (left < right) {
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if ((ch >= 'a' && ch <= 'z') || 
-                (ch >= '0' && ch <= '9')) {
-                clean = clean + ch;
+           
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
             }
+
+           
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+           
+            if (Character.toLowerCase(s.charAt(left)) != 
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
         }
 
-        // Reverse the string
-        String reverse = "";
-
-        for (int i = clean.length() - 1; i >= 0; i--) {
-            reverse = reverse + clean.charAt(i);
-        }
-
-        // Compare
-        return clean.equals(reverse);
+        return true;
     }
 }
